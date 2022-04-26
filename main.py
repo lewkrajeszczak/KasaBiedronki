efd34a8589c14979114d42bbf83a703edc119a39
lewkrajeszczak <95691507+lewkrajeszczak@users.noreply.github.com>
2022-04-26 16:17:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-banknoty = [500,200,100,50,20,10,5,2,1]
-money = int(input())
+import random
+banknoty = [500, 200, 100, 50, 20, 10, 5, 2, 1,0.50,0.20, 0.10, 0.05, 0.02, 0.01]
+cena = random.randint(1,1000)
 print('Czy posiadasz karte biedronki?')
-if input()=="TAK":
-    money -= money * 0.1
+ODP = input()
+if ODP in ["TAK", "tak","Tak", "posiadam", " tak posiadam", "jasne","tak poproszę"]:
+    cena -= cena * 0.1
     print('Rabat 10% został odjęty od rachunku')
-    print('do zapłaty', money, sep=' ')
+    print('do zapłaty', cena, sep=' ')
 
+pieniadzeklienta = round(float(input()),2)
+money = pieniadzeklienta - cena
 Change = []
+
+
 
 def run(money):
     for obj in banknoty:
@@ -15,5 +21,7 @@ def run(money):
             Change.append(obj)
             run(money)
             return
+
+
 run(money)
-print('to twoja reszta', Change, 'ZŁ' , sep=' ')
+print('to twoja reszta', Change, 'ZŁ', sep=' ')
